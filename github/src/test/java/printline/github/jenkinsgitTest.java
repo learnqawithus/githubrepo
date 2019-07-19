06856b3da77ae18f0279a627903a92e4b4d158a7
Fawad Anwar <Fawad Anwar@10.0.0.76>
2019-07-19 18:40:15 -0500

    Second Commit

--- github/src/test/java/printline/github/jenkinsgitTest.java
+++ github/src/test/java/printline/github/jenkinsgitTest.java
@@ -1,5 +1,12 @@
 package printline.github;
 
+import org.testng.annotations.Test;
+
 public class jenkinsgitTest {
+	
+	@Test
+	public void test1 () {
+		System.out.println("This is from jenkinsgitTest class");
+	}
 
 }
