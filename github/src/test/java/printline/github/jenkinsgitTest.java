42a1854d3d2a221e1d460219d7060ca8e926312e
Fawad Anwar <Fawad Anwar@10.0.0.76>
2019-07-19 18:57:55 -0500

    Third Commit

--- github/src/test/java/printline/github/jenkinsgitTest.java
+++ github/src/test/java/printline/github/jenkinsgitTest.java
@@ -1,12 +1,25 @@
 package printline.github;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class jenkinsgitTest {
 	
+	WebDriver driver;
+	
 	@Test
-	public void test1 () {
-		System.out.println("This is from jenkinsgitTest class");
+	public void GoogleTest () {
+		System.setProperty("webdriver.chrome.driver", "C:/Users/Fawad Anwar/Documents/drivers/chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		Assert.assertTrue(driver.getTitle().contains("Google"));
 	}
 
 }
